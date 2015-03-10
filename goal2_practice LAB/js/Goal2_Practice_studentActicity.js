8b192f3310b66519089bd6d097858d7c99b6ddcf
chrisfortin16 <Chris@Chriss-School-MacBook-Pro.local>
2015-03-10 14:07:29 -0400

    working with for loops

--- goal2_practice LAB/js/Goal2_Practice_studentActicity.js
+++ goal2_practice LAB/js/Goal2_Practice_studentActicity.js
@@ -127,10 +127,7 @@ var myArry = [
             'Woverine',
             'Iceman'
             ];
-    for (i = 0; i<=3; i++){
-        console.log(myArry);
-    }
-    for (i = 0; max-myArry.length; i++){
+    for (i = 0; i < myArry.length; i++){
         console.log(myArry);
     }
 
