fc41ed981e7c803033ee182954f0fc47d9bc4634
chrisfortin16 <Chris@Chriss-School-MacBook-Pro.local>
2015-03-10 11:27:16 -0400

    Working with strings

--- goal2_practice_DEMO_Fortin_Chris/js/Goal2_Practice_Demo.js
+++ goal2_practice_DEMO_Fortin_Chris/js/Goal2_Practice_Demo.js
@@ -30,7 +30,7 @@ console.log(str1.length);
 // Use split() to console.log an array of the ABC's using the abc variable.
 // EX: ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 
-
+console.log(abc.split(""));
 
 /*
 	===============================================
