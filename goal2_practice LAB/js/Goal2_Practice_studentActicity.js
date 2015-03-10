e6b813cfdc4fc610a185bd5082dc7e85299a39b4
chrisfortin16 <Chris@Chriss-School-MacBook-Pro.local>
2015-03-10 13:00:29 -0400

    working

--- goal2_practice LAB/js/Goal2_Practice_studentActicity.js
+++ goal2_practice LAB/js/Goal2_Practice_studentActicity.js
@@ -11,7 +11,7 @@ var abc = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 
 
 
-console.log(str1.charAt(7));
+console.log(str1.charAt(7,15));
 
 
 /*******************************************
