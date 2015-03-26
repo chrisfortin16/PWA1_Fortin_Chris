67e64cc9bc3d0a087f83bb0ab5b4e722acb2fe43
chrisfortin16 <Chris@Chriss-School-MacBook-Pro.local>
2015-03-26 09:32:28 -0400

    starting the final

---
+++ OPTION_2_assign_FINAL/js/main.js
@@ -0,0 +1,15 @@
+/*
+	* Mid Terms for PWA-1
+*/
+
+var person = {
+    name: bond,
+    gpa: [2.5, 3.5, 4.0],
+    address: {
+        street: '123 ave drive',
+        city: 'Miami',
+        state: 'Florida'
+    }
+};
+
+console.log(person);
