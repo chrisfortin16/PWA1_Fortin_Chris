071126f9c28ffc453684f7e56af275974e6984f2
chrisfortin16 <Chris@Chriss-School-MacBook-Pro.local>
2015-03-26 10:20:43 -0400

    working with java final

--- OPTION_2_assign_FINAL/js/main.js
+++ OPTION_2_assign_FINAL/js/main.js
@@ -1,15 +1,50 @@
 /*
 	* Mid Terms for PWA-1
+	* Chris Fortin
+	* 03/26/15
 */
 
 var person = {
-    name: bond,
+    name: "JBond",
     gpa: [2.5, 3.5, 4.0],
     address: {
-        street: '123 ave drive',
-        city: 'Miami',
+        street: '3300 University Blv',
+        city: 'Winter Park',
         state: 'Florida'
     }
 };
 
-console.log(person);
+var newPerson = {
+    name:'super man',
+    Gpa:[3.2, 4.0, 2.2],
+    address: {
+        street: '123 Test Dr',
+        city: 'Orlando',
+        state: 'Florida'
+    }
+};
+
+var thisArray = [person,newPerson,addData()];
+
+console.log(person.name);
+console.log(person.gpa);
+console.log(person.address.street);
+console.log(person.address.city);
+console.log(person.address.state);
+
+console.log(newPerson.name);
+console.log(newPerson.gpa);
+console.log(newPerson.address.street);
+console.log(newPerson.address.city);
+console.log(newPerson.address.state);
+
+
+var addData = function(){
+
+    document.innerHTML(person);
+    document.innerHTML(newPerson);
+
+};
+
+button.onclick = addData();
+
